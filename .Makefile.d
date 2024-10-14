802cf52938be45cae310ad4595229515e86e2fab
NotBad4U <lightplay8@gmail.com>
2024-10-14 15:32:50 +0200

    Remove Heap.v

--- .Makefile.d
+++ .Makefile.d
@@ -4,7 +4,5 @@ theories/Inc.vo theories/Inc.glob theories/Inc.v.beautified theories/Inc.require
 theories/Inc.vos theories/Inc.vok theories/Inc.required_vos: theories/Inc.v theories/StringAsOT.vos theories/StateMap.vos
 theories/Hoare.vo theories/Hoare.glob theories/Hoare.v.beautified theories/Hoare.required_vo: theories/Hoare.v 
 theories/Hoare.vos theories/Hoare.vok theories/Hoare.required_vos: theories/Hoare.v 
-theories/Heap.vo theories/Heap.glob theories/Heap.v.beautified theories/Heap.required_vo: theories/Heap.v 
-theories/Heap.vos theories/Heap.vok theories/Heap.required_vos: theories/Heap.v 
 theories/StringAsOT.vo theories/StringAsOT.glob theories/StringAsOT.v.beautified theories/StringAsOT.required_vo: theories/StringAsOT.v 
 theories/StringAsOT.vos theories/StringAsOT.vok theories/StringAsOT.required_vos: theories/StringAsOT.v 
